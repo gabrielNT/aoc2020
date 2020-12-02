2718da29f0500c11b635dacbbd4dc4ebad6f40d8
Gabriel Tutui <gntutui@gmail.com>
2020-12-01 21:57:55 -0300

    Day 1 solutions
    Took a very lazy approach for this one. This is basically a 2sum/3sum
    problem, solved using HashSets. Maybe I could also have ordered it and
    used two pointers.

---
+++ src/day1.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+
+// Not parsing as a generator since HashSet doesn't have as_ref
+pub fn parse_input(input: &str) -> HashSet<i64> {
+    input
+        .lines()
+        .map(|l| l.parse::<i64>().unwrap())
+        .collect::<HashSet<_>>()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &str) -> i64 {
+    let entries = parse_input(input);
+
+    for i in entries.iter() {
+        if entries.contains(&(2020 - i)) {
+            return i * (2020 - i);
+        }
+    }
+
+    panic!("No two entries have a sum of 2020")
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> i64 {
+    let entries = parse_input(input);
+
+    for i in entries.iter() {
+        // Taking the lazy approach here, but this will have issues since we may consider 
+        // the same number twice
+        for j in entries.iter() {
+            let target = 2020 - i - j;
+
+            if entries.contains(&target) {
+                return i * j * target;
+            }
+        }
+    }
+
+    panic!("No three entries have a sum of 2020")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,6 @@ extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
 
-aoc_lib!{ year = 2020 }
+pub mod day1;
+
+aoc_lib! { year = 2020 }
